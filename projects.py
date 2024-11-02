0c33befcb68faa6ed5b8f17ebe242effa7fced1b
SionaGupta <siona.gupta00@gmail.com>
2024-11-02 16:18:24 -0700

    Update project.py
    Testing adding a buy milk task to every project

--- projects.py
+++ projects.py
@@ -5,8 +5,7 @@ import os
 load_dotenv()  # Loads variables from .env into the environment
 
 print(os.getenv("test_key"))
-
-api = TodoistAPI(os.environ.get('TODOSIT_API_KEY'))
+api = TodoistAPI(os.environ.get('TODOIST_API_KEY'))
 
 
 try:
@@ -14,6 +13,11 @@ try:
 
     for project in projects:
         print(project.name)
+        try:
+            task = api.add_task(content="Buy Milk", project_id=project.id)
+            print(task)
+        except Exception as error:
+            print(error)
 
     # print(projects)
 except Exception as error:
