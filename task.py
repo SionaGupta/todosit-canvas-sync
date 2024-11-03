3f97181eb3e2b376efae5240e40f4618f2f2a141
SionaGupta <siona.gupta00@gmail.com>
2024-11-02 19:07:41 -0700

    Created task.py
    Started todoist task class implementation

---
+++ task.py
@@ -0,0 +1,31 @@
+class TodositTask:
+    def __init__(self, id, name, is_completed, priority) -> none:
+        self.id = id
+        self.name = name
+        self.is_completed = is_completed
+        self.priority = priority
+
+    def bark(self):
+        return f"{self.name} says woof!"
+
+    def info(self):
+        return f"{self.name} is a {self.breed}."
+
+
+     id: "2995104339",
+    content: "Buy Milk",
+    description: "",
+    comment_count: 0,
+    is_completed: False,
+    order: 1,
+    priority: 1,
+    project_id: "2203306141",
+    labels: [],
+    due: None,
+    section_id: None,
+    parent_id: None,
+    creator_id: "2671355",
+    created_at: "2019-12-11T22:36:50.000000Z",
+    assignee_id: None,
+    assigner_id: None,
+    url: "https://todoist.co
