6c5e6902bd34e6214f2028086ed879e249ddd9b0
SionaGupta <siona.gupta00@gmail.com>
2024-11-03 01:56:46 -0700

    Working API calls

--- canvas.py
+++ canvas.py
@@ -5,3 +5,6 @@ import os
 load_dotenv()  # Loads variables from .env into the environment
 
 CANVAS_API_KEY  = os.environ.get('CANVAS_API_KEY')
+params = {'appid': CANVAS_API_KEY} 
+
+r = requests.get('https://<canvas>/api/v1/users/self/courses', params=params)
