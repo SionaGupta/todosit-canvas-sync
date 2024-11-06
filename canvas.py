17ba81e9c0c2243a3b1575b55876443bdb697d33
SionaGupta <siona.gupta00@gmail.com>
2024-11-05 18:27:17 -0800

    Working API Calls
    Fixed typos and added try: except error

--- canvas.py
+++ canvas.py
@@ -5,10 +5,12 @@ import os
 load_dotenv()  # Loads variables from .env into the environment
 
 CANVAS_API_KEY  = os.environ.get('CANVAS_API_KEY')
-print(CANVAS_API_KEY)
 
 
 s = requests.Session()
-s.headers.update({'Authorization': 'Bearer {CANVAS_AP I_KEY}'})
+s.headers.update({'Authorization': 'Bearer {CANVAS_API_KEY}'})
 
-r = s.get('https://<canvas>/api/v1/courses')
+try:
+    r = s.get('https://<canvas>/api/v1/courses')
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    raise SystemExit(e)
