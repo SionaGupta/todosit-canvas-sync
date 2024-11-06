06c8ca45e0ec29251b9947fc887b30816d43bdd9
SionaGupta <siona.gupta00@gmail.com>
2024-11-05 17:49:17 -0800

    Working API Calls
    Made request session

--- canvas.py
+++ canvas.py
@@ -5,6 +5,10 @@ import os
 load_dotenv()  # Loads variables from .env into the environment
 
 CANVAS_API_KEY  = os.environ.get('CANVAS_API_KEY')
-params = {'appid': CANVAS_API_KEY} 
+print(CANVAS_API_KEY)
 
-r = requests.get('https://<canvas>/api/v1/users/self/courses', params=params)
+
+s = requests.Session()
+s.headers.update({'Authorization': 'Bearer {CANVAS_AP I_KEY}'})
+
+r = s.get('https://<canvas>/api/v1/courses')
