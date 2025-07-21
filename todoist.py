cfe2be23d0d0959f36cf73863bcbb422f12d5691
SionaGupta <siona.gupta00@gmail.com>
2025-07-20 18:50:50 -0700

    Working

--- todoist.py
+++ todoist.py
@@ -12,6 +12,7 @@ load_dotenv()  # Loads variables from .env into the environment
 print(os.getenv("test_key"))
 api = TodoistAPI(os.environ.get('TODOIST_API_KEY'))
 
+
 def add_assignments(assignments, course_name):
     
     section_id = 1
@@ -51,13 +52,13 @@ def add_assignments(assignments, course_name):
  
     for assignment in assignments: 
         add_oneassignment(assignment, project_id, section_id)
-    
+
+
 def time_pst(date):
     utc_dt = datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
     utc_dt = pytz.utc.localize(utc_dt)
     california_dt = utc_dt.astimezone(california_tz)
     california_dt = str(california_dt)
-    print(california_dt)
     return california_dt
 
 
@@ -65,48 +66,52 @@ def add_oneassignment(assignment, project_id, section_id):
     # wororkeokp
     try:
         duedate = assignment['due_at']
+        #adjust time
         duedate = time_pst(duedate);
-        date = duedate[:15]
+        #remove time
+        date = duedate[:10]
         print(assignment['name'] + " " + duedate)
 
         sub = assignment['submission']
-      
-        print(sub['attempt'])
-        print(sub['attempt']!= None)
-        active = sub['attempt']!= None
+        active = sub['attempt'] == None
         
+        print(date)
+
         task_data = {
             'content': assignment['name'],  # Task content
-            'project_id': project_id,       # Project ID
-            'section_id': section_id,       # Section ID
+            'project_id': str(project_id),       # Project ID
+            'section_id': str(section_id),       # Section ID
             'due_date': date,  # Task due date
             'priority': 2,
-            'is_completed': active
         }
 
         alltasks = api.get_tasks(project_id=project_id, section_id=section_id)
+    
 
-     
-        
         #Check if current task is already listed
-        for alltask in alltasks:
-            if (assignment['name'] == alltask.content): 
-                #if listed, check if completion is the same
-                if not (active == alltask.is_completed):
-                    task_id = alltask.id 
-                    #update task if different
-                    update = api.update_task(
-                        task_id=task_id,
-                        is_completed = active)
-                    print(update)
-            else: 
-                #add task
-                task = api.add_task(**task_data)
-                print(task)
-                
+        if alltasks:
+            for task in alltasks: 
+                print(task.content)
+
+            for alltask in alltasks:
+                if (assignment['name'] == alltask.content):        
+                    print('found')
+
+                    #if listed, check if task is completed since last update     
+                    if (active == False):
+                        task_id = alltask.id 
+                        #update task if different
+                        update = api.close_task(task_id=task.id)
+                        print(update)
+        else: 
+            print('not found')
+            #add task
+            if (active == True):
+                task = api.add_task(**task_data)                
             
     except Exception as error:
         print(error) 
+
 
 def test_sections():
 
@@ -118,3 +123,19 @@ def test_sections():
             print(section)
         except Exception as error:
             print(error)
+
+
+def test_tasks(): 
+    projects = api.get_projects()
+
+    for project in projects: 
+        if (project.name == "College Classes"):
+            sections = api.get_sections()
+            for section in sections: 
+                if (section.name == 'MATH 270 : Linear Algebra - Zheng A. - SUMMER 2025 - SECTION# 10425'):
+                    try:
+                        task = api.add_task(content="hello World", project_id=project.id, section_id=section.id)
+                        print(task)
+                    except Exception as error: 
+                        print(error)
+
